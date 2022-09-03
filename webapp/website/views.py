c5e5650190d6465a4edb350573ebb6f0be5bf964
YUJBAI <yujie.bai@student.unimelb.edu.au>
2022-09-04 02:00:34 +1000

    Move Edit button to view page

--- webapp/website/views.py
+++ webapp/website/views.py
@@ -11,71 +11,10 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    #bef_data = None
-    #aft_data = None
-    INPUTFORMAT = False
-    if request.method == 'POST':
-        instrument = request.form.get("instrument")
-        dataGenerated = request.form.get('dataGenerated')
-        price = request.form.get('price')
-        lifetime = request.form.get('lifetime')
-        startDate = request.form.get('startDate')
-        initialSize = request.form.get('initialSize')
-
-        if instrument == "":
-            flash("Please fill in the Instrument's name!", category='error')
-        elif dataGenerated == "":
-            flash("Please fill in the Data Generated Per Year!", category= "error")
-        elif not isFloat(dataGenerated):
-            flash("Please make sure Data Generated Per Year is a number!", category= "error")
-        elif price == "" or not isFloat(price):
-            flash("Please fill in the Instrument's Price!", category= "error")
-        elif not isFloat(price):
-            flash("Please make sure Instrument's Price is a number!", category= "error")
-        elif lifetime == "" or not isFloat(lifetime):
-            flash("Please fill in the Expected Lifetime!", category= "error")
-        elif not isFloat(lifetime):
-            flash("Please make sure the Expected Lifetime is a number!", category= "error")
-        elif startDate == "":
-            flash("Please fill in the Start Date!", category= "error")
-        elif not check_date(startDate):
-            flash("Please make sure Start Date is in the correct format and is in the future!", category= "error")
-        elif initialSize == "":
-            flash("Please fill in the Initial Data Size!", category= "error")
-        elif not isFloat(initialSize):
-            flash("Please make sure Initial Data Size is a number!", category= "error")
-        else:
-            INPUTFORMAT = True
-            new_history = History(name = instrument,
-                            data_generated = float(dataGenerated),
-                            price = float(price),
-                            lifetime = float(lifetime),
-                            start_date = startDate,
-                            initial_size = float(initialSize),
-                            user_id=current_user.id)
-            db.session.add(new_history)
-            db.session.commit()
-            print(new_history)
-            flash('Done Calculating!', category='success')
-
-    if current_user.histories:
-        input_user = current_user.histories[-1]
-        bef_data, aft_data = generate_graph_data(start_date = input_user.start_date, 
-                                    data_generated_per_year = input_user.data_generated * 365, 
-                                    year = 5,
-                                    initial_data_size = input_user.initial_size,
-                                    gradient = input_user.initial_size * 1 / 3)
-    else:
-        bef_data = aft_data = None
-
-    if INPUTFORMAT:
-        return render_template("home.html", user=current_user, before = bef_data, after = aft_data)
-    else:
-        return render_template("home.html", user=current_user, before=bef_data, after=aft_data, form=request.form)
+    return render_template("home.html", user=current_user)
 
 @views.route('/insert', methods=['GET', 'POST'])
 def insert():
-    print('hiiiiiiiiiiii')
     if request.method == 'POST':
         instrument = request.form.get("instrument")
         dataGenerated = request.form.get('dataGenerated')
@@ -123,17 +62,16 @@ def setting():
 def view_history(history_id):
     input_user = History.query.filter_by(id=history_id).first()
     if input_user:
-        bef_data, aft_data = generate_graph_data(start_date = input_user.start_date,
+        data_using_linear_model_without_instrument, data_using_linear_model_with_instrument = generate_graph_data(start_date = input_user.start_date,
                                     data_generated_per_year = input_user.data_generated * 365,
                                     year = 5,
                                     initial_data_size = input_user.initial_size,
                                     gradient = input_user.initial_size * 1 / 3)
     else:
         history_id=0
-        bef_data = aft_data = None
-    # print(type(history_id))
-    # print(input_user.id)
-    return render_template("view_history.html", user=current_user, before = bef_data, after = aft_data, history_id=input_user.id)
+        data_using_linear_model_without_instrument = data_using_linear_model_with_instrument = None
+    return render_template("view_history.html", user=current_user, before = data_using_linear_model_without_instrument,
+                           after = data_using_linear_model_with_instrument, history_id=input_user.id)
 
 
 
@@ -163,7 +101,6 @@ def delete_storage():
 
 @views.route('/update', methods=['POST','GET'])
 def update():
-        print(request.form)
         history_id = request.form['id']
         name = request.form['name']
         data_generated = request.form['data_generated']
@@ -171,16 +108,10 @@ def update():
         lifetime = request.form['lifetime']
         start_date = request.form['start_date']
         initial_size = request.form['initial_size']
-        print(name)
         history_id = int(history_id)
-        history = History.query.get(history_id)
-        print(type(history))
-        print(history.name)
         History.query.filter_by(id=history_id).update({'name': name, 'data_generated': data_generated, 'price': price,
                                                        'lifetime': lifetime, 'start_date': start_date, 'initial_size': initial_size})
         db.session.commit()
-        history = History.query.filter_by(id=history_id).first()
-        print(history.name)
         succuss = 1
         return jsonify(succuss)
 
@@ -213,8 +144,3 @@ def modal(history_id):
 
 #     return jsonify({})
 
-# @views.route('/test/<history_id>',methods=['POST','GET'])
-# def test(history_id):
-#     history = History.query.filter_by(id=history_id).first()
-#     name = history.name
-#     return render_template("teset.html", name=name)
